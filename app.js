23423e698fd129f6d9412278c5c547739e49d66c
TheB1910577 <tlbm133@gmail.com>
2022-09-22 21:09:01 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-
+const ApiError = require("./app/api-error");
 const app = express();
-
+const contactsRouter = require("./app/routes/contact.route");
 app.use(cors());
 app.use(express.json());
 
@@ -10,5 +10,19 @@ app.get("/" , (req, res) => {
 	res.json({message: "Welcome to contact book application." });
 });
 
+app.use("/api/contacts", contactsRouter);
+
+app.use("/api/contacts",(req, res, next) => {
+	return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((error, req, res, next) => {
+	return res.status(error.statusCode || 500).json({
+		message: error.message || "Resource not found",
+	});
+});
+
 module.exports = app;
+
+
 
